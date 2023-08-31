d751061d89f4a0f7adbbb0651f3dfa2f5baac591
hmcdl <124053218+hmcdl@users.noreply.github.com>
2023-08-31 17:06:14 +0300

    Add files via upload

--- Optimization.py
+++ Optimization.py
@@ -3,12 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.optimize import Bounds
+from typing import List, Set, Dict, Tuple
+
 
 
 def rosen(x):
     """The Rosenbrock function"""
-    return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+    return np.sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
+def rosen2D(x1, x2):
+    return 100*(x2-x1**2)**2+(x1-1)**2
+
+def cantileverBeamArea(x):
+    w = x[0]
+    t = x[1]
+    return 4*t*(w-t)
 
 def rosen_der(x):
     xm = x[1:-1]
@@ -32,6 +41,74 @@ def rosen_der(x):
 #            'fun' : lambda x: np.array([2*x[0] + x[1] - 1]),
 #            'jac' : lambda x: np.array([2.0, 1.0])}
 
+#Constants:
+L = 2000
+P = 20000
+q_a = 10
+Tau_a = 90
+Sigma_a = 165
+E = 21*10**4
+
+
+def J(x):
+    w = float(x[0])
+    t = float(x[1])
+    return 1/12*(w**4 - (w-2*t)**4) 
+
+def Q(x):
+    w = float(x[0])
+    t = float(x[1])
+    return 1/8 * w**3 - 1/8*(w-2*t)**3 - 90
+
+def maxNormal(x):
+    w = float(x[0])
+    t = float(x[1])
+    J_ = J(x)
+    sig = P*L*w/(2*J_)
+    return P*L*w/(2*J_) - 165
+
+def maxShear(x):
+    w = float(x[0])
+    t = float(x[1])
+    return P*Q(x) / (2*J(x)*t) - 90
+
+def maxDeflection(x):
+    w = float(x[0])
+    t = float(x[1])
+    maxDef = P*L**3/(3*E*J(x))
+    J_val = J(x)
+    return P*L**3/(3*E*J(x)) - 10
+
+def dim_relation(x):
+    w = float(x[0])
+    t = float(x[1])
+    return w - 8*t
+
+def dim_relation2(x):
+    w = float(x[0])
+    t = float(x[1])
+    return 2*t-w
+
+ineq_Max_Normal = {'type': 'ineq', 
+                   'fun' :maxNormal
+                   }
+
+ineq_max_Shear = {'type': 'ineq', 
+                   'fun' :maxShear
+                   }
+
+ineq_max_Deflection = {'type': 'ineq', 
+                   'fun' :maxDeflection
+                   }
+
+ineq_dim_relation = {'type': 'ineq', 
+                   'fun' :dim_relation
+                   }
+
+ineq_dim_relation2 = {'type': 'ineq', 
+                   'fun' :dim_relation2
+                   }
+
 ineq_cons = {'type': 'ineq',
              'fun' : lambda x: np.array([1 - x[0] - 2*x[1],
                                          1 - x[0]**2 - x[1],
@@ -44,21 +121,43 @@ eq_cons = {'type': 'eq',
            'fun' : lambda x: np.array([2*x[0] + x[1] - 1]),
            'jac' : lambda x: np.array([2.0, 1.0])}
 
-# x1 = np.linspace(-1, 1, 50)
+# x1 = np.linspace(1, 20, 50)
+# x2 = np.linspace(0.5, 2, 50)
 
-# X, Y = np.meshgrid(x1, x1)
+
+# X, Y = np.meshgrid(x1, x2)
+# Z = cantileverBeamArea(X, Y)
 
 # ax = plt.subplot(111, projection='3d')
 
-# ax.plot_surface(X, Y, rosen([X, Y])[0])
+# ax.plot_surface(X, Y, Z)
 
 # plt.show()
 
-# x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
-x0 = np.array([0.5, 0])
-# bounds = Bounds([0,-1,-1,-2,0], [2,1,1,3,2])
-bounds = Bounds([0, -0.5], [1.0, 2.0])
-res = minimize(rosen, x0, method='SLSQP', jac='3-point', #
-                constraints=[eq_cons, ineq_cons], options={'ftol': 1e-9, 'disp': True}, #
+x0 = np.array([60, 10])
+A0 = cantileverBeamArea(x0)
+J_value = J(x0)
+Q_value = Q(x0)
+Sigma = maxNormal(x0) + 165
+Tau = maxShear(x0) + 90
+q = maxDeflection(x0) + 10
+rel = dim_relation(x0)
+
+print (Sigma, Tau, q)
+
+bounds = Bounds([10, 1], [200, 400])
+res = minimize(rosen, x0, method='SLSQP', jac='2-point', #
+                constraints=[ineq_Max_Normal, ineq_max_Shear, ineq_max_Deflection, ineq_dim_relation, ineq_dim_relation2], #
+                options={'ftol': 1e-9, 'disp': True}, #
                bounds=bounds)
 print(res.x)
+x0 = res.x
+A0 = cantileverBeamArea(x0)
+J_value = J(x0)
+Q_value = Q(x0)
+Sigma = maxNormal(x0) + 165
+Tau = maxShear(x0) + 90
+q = maxDeflection(x0) + 10
+rel = dim_relation(x0)
+print (Sigma, Tau, q)
+a=1
